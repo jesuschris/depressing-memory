7dfb157b334880df101309305e067b2cfb477c3e
chris <christopherstruble@gmail.com>
2016-05-27 23:23:05 +0000

    updated interface

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -5,8 +5,8 @@ app.controller("AppController", function($scope, appState, socket){
 
 app.controller("PageController", function($scope, $window){
   var setWH = function(){
-    $scope.width = $window.innerWidth + "px";
-    $scope.height = $window.innerHeight + "px";
+    $scope.width = $window.innerWidth  + "px";
+    $scope.height = ($window.innerHeight - 16) + "px";
     //console.log($scope.width + " x " + $scope.height);
   };
   angular.element($window).bind('resize', function(){
@@ -73,15 +73,22 @@ app.controller("CardsAreaController", function($scope, $state, $window, $http, $
     if($window.innerWidth >= $window.innerHeight){
       if($window.innerWidth >= 765){
         //Bigger landscape... make font a little bigger!
-        $scope.fontSize = (1.5 * ($window.innerWidth / 100)) + "px";
+        $scope.fontSize = (1.3 * ($window.innerWidth / 100)) + "px";
       }
       else{
-        //Small andscape... make font small!
+        //Small landscape... make font small!
         $scope.fontSize = (2 * ($window.innerWidth / 100)) + "px";
       }
     }
     else{
-      $scope.fontSize = (2.5 * ($window.innerWidth / 100)) + "px";
+      if($window.innerWidth >= 599){
+        //Bigger portrait... make font a little bigger!
+        $scope.fontSize = (1.5 * ($window.innerWidth / 100)) + "px";
+      }
+      else{
+        //Small portrait... make font small!
+        $scope.fontSize = (2.5 * ($window.innerWidth / 100)) + "px";
+      }
     }
   };
 
@@ -91,29 +98,43 @@ app.controller("CardsAreaController", function($scope, $state, $window, $http, $
   });
   setFontSize();
 
+  $scope.isLoading = false;
   $scope.isReady = false;
   $scope.isPlaying = false;
   $scope.canFlip = false;
 
+  $scope.flipCount = 0;
   $scope.matchedPairs = 0;
 
+  $scope.timeLeft = appState.getDifficulty().timeLimit;
+
   $scope.cards = [];
+  var flippedCards = [];
+
   $scope.pendingCard = null;
 
   $scope.flipCard = function(card){
     if($scope.isReady && $scope.isPlaying && $scope.canFlip){
       if(!card.isFlipped){
+        $scope.flipCount++;
         card.isFlipped = true;
+        flippedCards.push(card);
         if($scope.pendingCard !== null){
           if($scope.pendingCard.title === card.title){
             $scope.matchedPairs++;
             $scope.pendingCard = null;
+            if($scope.matchedPairs === appState.getDifficulty().pairs){
+              //We're done!!
+              $scope.stopTimer();
+            }
           }
           else{
             $scope.canFlip = false;
             $timeout(function() {
               card.isFlipped = false;
               $scope.pendingCard.isFlipped = false;
+              flippedCards.pop();
+              flippedCards.pop();
               $scope.pendingCard = null;
               $scope.canFlip = true;
             }, 1000);
@@ -126,11 +147,73 @@ app.controller("CardsAreaController", function($scope, $state, $window, $http, $
     }
   }
 
-  var getCards = function(numPairs){
+  $scope.buttonText = "Start";
+  $scope.buttonClass = "md-primary";
+
+  var timer = null;
+  $scope.LessThan10SecondsLeft = false;
+
+  $scope.buttonAction = function(buttonText){
+    if(buttonText === "Start"){
+      $scope.buttonText = "Quit";
+      $scope.buttonClass = "md-warn";
+      $scope.isPlaying = true;
+      $scope.canFlip = true;
+      ($scope.startTimer =function() {
+        $scope.timeLeft -= 1000;
+        $scope.LessThan10SecondsLeft = $scope.timeLeft <= 10000 ? true : false;
+        timer = $timeout($scope.startTimer, 1000);
+        if ($scope.timeLeft === 0) {
+          $scope.stopTimer();
+          $scope.canFlip = false;
+        }
+      })();
+    }
+    else if(buttonText === "Quit"){
+      $scope.stopTimer();
+    }
+    else if(buttonText === "Reset"){
+      resetGame();
+      setUpGame(appState.getDifficulty());
+    }
+    else{
+      //???????
+    }
+  }
+
+  $scope.stopTimer = function() {
+    $scope.buttonText = "Reset";
+    $scope.buttonClass = "md-accent";
+    $timeout.cancel(timer);
+    $scope.isPlaying = false;
+  }
+
+  var resetGame = function(){
+    $scope.flipCount = 0;
+    $scope.matchedPairs = 0;
+
+    $scope.isReady = false;
+    $scope.isPlaying = false;
+    $scope.canFlip = false;
+
+    angular.forEach(flippedCards, function(card){
+      if(card.isFlipped){
+        card.isFlipped = false;
+      }
+    });
+    flippedCards = [];
+
+    $scope.buttonText = "Start";
+    $scope.buttonClass = "md-primary";
+  }
+
+  var setUpGame = function(difficulty){
+    $scope.isReady = false;
+    $scope.isLoading = true;
     $http({
       url: "api/cards",
       method: "GET",
-      params: { num: numPairs }
+      params: { num: difficulty.pairs }
     })
       .then(function(res){
         var data = res.data;
@@ -180,22 +263,30 @@ app.controller("CardsAreaController", function($scope, $state, $window, $http, $
         cards = shuffleCards(cards);
         $scope.cards = cards;
         $scope.isReady = true;
-        $scope.isPlaying = true;
-        $scope.canFlip = true;
+        $scope.timeLeft = difficulty.timeLimit;
+        //Just for asthetics ;)
+        $timeout(function(){
+          $scope.isLoading = false;
+        }, 500);
       })
       .catch(function(err){
         console.log(err);
       });
     };
 
+    $scope.goToMenu = function(){
+        $state.go("menu");
+    }
+
     appState.observe(function(message){
 
     });
 
     if(!appState.getIsUserSetup()){
-      $state.go("menu");
+      $scope.goToMenu();
     }
     else{
-      getCards(appState.getDifficulty().pairs);
+      resetGame();
+      setUpGame(appState.getDifficulty());
     }
 });
